d5f5212f7f28b4852bbb2e9cd6358fe9b36ccd0c
CheFly1116 <48715037+CheFly1116@users.noreply.github.com>
2023-05-29 21:28:19 +0900

    login added

--- backend/mainApp/routes.py
+++ backend/mainApp/routes.py
@@ -1,8 +1,9 @@
-from controllers.example_controllers import example_bp, openai_bp, search_bp, query_bp
+from controllers.example_controllers import example_bp, openai_bp, search_bp, query_bp, login_bp
 
 def routes_list(app):
     app.register_blueprint(example_bp)
     app.register_blueprint(openai_bp)
     app.register_blueprint(search_bp)
     app.register_blueprint(query_bp)
+    app.register_blueprint(login_bp)
     return app
