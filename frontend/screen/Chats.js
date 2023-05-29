8f0a1363342ff6a0a96331bbd4687a635f9f3837
sudokim <khs1778@g.skku.edu>
2023-05-30 04:06:21 +0900

    Convert jsx elements to react-native elements

--- frontend/screen/Chats.js
+++ frontend/screen/Chats.js
@@ -1,22 +1,14 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useState} from 'react';
+import {Button, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 
-//const App = () => {
 function Chats({navigation}){
   const [messages, setMessages] = useState([]);
   const [inputText, setInputText] = useState('');
 
-  const chatContainerRef = useRef(null);
-  const inputRef = useRef(null);
-
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-
-  const handleMessageSubmit = (e) => {
-    e.preventDefault();
+  const handleMessageSubmit = () => {
     const message = inputText.trim();
     if (message !== '') {
-      setMessages([...messages, { text: message, isUser: true }]);
+      setMessages([...messages, {text: message, isUser: true}]);
       setInputText('');
       sendMessageToChatGPT(message);
     }
@@ -34,121 +26,99 @@ function Chats({navigation}){
       });
       if (response.ok) {
         const data = await response.json();
-        // data로 채팅창에 답변 띄우는 과정 필요, 구현 부탁드립니다
+        // TODO: display the response data in the chat
       } else {
         const errorData = await response.json();
-        // 오류 처리 부분입니다
+        throw new Error(`Server responded with ${response.status}: ${errorData.error}`);
       }
     } catch (error) {
       console.error('Error', error);
     }
   };
 
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      handleMessageSubmit(e);
-    }
-  };
-
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100vh',
-        width: '100%',
-      }}
-    >
-
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '10%',
-          backgroundColor: '#072A60',
-          color: '#FFFFFF',
-          fontSize: '32px',
-          fontWeight: 'bold',
-        }}
-      >
-        <text>SKKU-GPT</text>
-      </div>
-
-
-      <div
-        style={{
-          flex: '8',
-          backgroundColor: '#B6BFD0',
-          padding: '20px',
-          overflowY: 'auto',
-        }}
-      >
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            style={{
-              display: 'flex',
-              justifyContent: message.isUser ? 'flex-end' : 'flex-start',
-              marginBottom: '10px',
-            }}
-          >
-            <div
-              style={{
-                maxWidth: '70%',
-                backgroundColor: message.isUser ? '#FFFFFF' : '#000000',
-                color: message.isUser ? '#000000' : '#FFFFFF',
-                padding: '10px',
-                borderRadius: '8px',
-              }}
-            >
-              <text>{message.text}</text>
-            </div>
-          </div>
-        ))}
-      </div>
-
- 
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          height: '50px',
-
-          backgroundColor: '#FFFFFF',
-        }}
-      >
-        <input
-          type="text"
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-          ref={inputRef}
-          style={{
-            flex: '1',
-            border: 'none',
-	height: '50px',
-            borderRadius: '0',
-          }}
-          placeholder="Ask anything"
-          onKeyPress={handleKeyPress}
-        />
-        <button
-          type="submit"
-          style={{
-            backgroundColor: '#072A60',
-            color: '#FFFFFF',
-            border: 'none',
-            borderRadius: '0',
-	width: '100px',
-            height: '52px',
-          }}
-          onClick={handleMessageSubmit}
-          title='Send'
-        >
-        </button>
-      </div>
-    </div>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>SKKU-GPT</Text>
+        </View>
+        <ScrollView style={styles.chatContainer}>
+          {messages.map((message, index) => (
+              <View
+                  key={index}
+                  style={[
+                    styles.messageContainer,
+                    {alignSelf: message.isUser ? 'flex-end' : 'flex-start'}
+                  ]}
+              >
+                <Text style={message.isUser ? styles.userMessage : styles.botMessage}>
+                  {message.text}
+                </Text>
+              </View>
+          ))}
+        </ScrollView>
+        <View style={styles.inputContainer}>
+          <TextInput
+              style={styles.input}
+              value={inputText}
+              onChangeText={(text) => setInputText(text)}
+              placeholder="Ask anything"
+              onSubmitEditing={handleMessageSubmit}
+          />
+          <Button title="Send" onPress={handleMessageSubmit}/>
+        </View>
+      </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#072A60',
+  },
+  headerText: {
+    color: '#FFFFFF',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  chatContainer: {
+    flex: 1,
+    backgroundColor: '#B6BFD0',
+    padding: 20,
+  },
+  messageContainer: {
+    maxWidth: '70%',
+    marginBottom: 10,
+  },
+  userMessage: {
+    backgroundColor: '#FFFFFF',
+    color: '#000000',
+    padding: 10,
+    borderRadius: 8,
+  },
+  botMessage: {
+    backgroundColor: '#000000',
+    color: '#FFFFFF',
+    padding: 10,
+    borderRadius: 8,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+    borderColor: '#072A60',
+    borderWidth: 1,
+    marginRight: 10,
+    borderRadius: 4,
+  },
+});
 
 export default Chats;
